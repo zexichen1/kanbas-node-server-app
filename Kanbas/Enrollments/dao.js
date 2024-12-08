ceb34ddb6834f1f9fdee9148e5681659c86a8f41
RickyCUHK <119010110@link.cuhk.edu.cn>
2024-12-07 19:28:13 -0800

    Update dao.js

--- Kanbas/Enrollments/dao.js
+++ Kanbas/Enrollments/dao.js
@@ -33,9 +33,6 @@ export async function findUsersForCourse(courseId) {
  const enrollments = await model.find({ course: courseId }).populate("user");
  return enrollments.map((enrollment) => enrollment.user);
 }
-export function enrollUserInCourse(user, course) {
- return model.create({ user, course });
-}
 export function unenrollUserFromCourse(user, course) {
  return model.deleteOne({ user, course });
 }
